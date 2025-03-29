4f4505ad46eacd3e371688ee36c99c4951d297ce
Mahad Munir <mahadmunir6@gmail.com>
2025-03-29 16:55:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 from keras.layers import TFSMLayer
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross Origin Resour
+CORS(app)  # Enable Cross Orig
 
 # Configure upload folder
 app.config['UPLOAD_FOLDER'] = 'uploads'
